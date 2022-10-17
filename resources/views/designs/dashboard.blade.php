5ae549f127e4551ff7ef6777b6cc451ee3da7492
Fathul Husnan <fathulhusnan9901@gmail.com>
2022-10-17 11:18:27 +0700

    dashboard page design done

--- resources/views/designs/dashboard.blade.php
+++ resources/views/designs/dashboard.blade.php
@@ -1,7 +1,71 @@
 @extends('layouts.main', ['title' => 'Dasbor'])
 
 @section('contents')
-    <x-card>
-        Tes
+    {{-- Summary --}}
+    <div class="row">
+        <div class="col-md-4 mb-3">
+            <x-card>
+                <div class="d-flex display-5 align-items-center">
+                    <div class="p-0 m-0 text-primary">
+                        <i class="bx bxs-user-account"></i>
+                    </div>
+                    <div class="border-start ps-3 ms-3">
+                        <span id="total-agent">0</span><br>
+                        <small class="h6">Total Agen</small>
+                    </div>
+                </div>
+            </x-card>
+        </div>
+
+        <div class="col-md-4 mb-3">
+            <x-card>
+                <div class="d-flex display-5 align-items-center">
+                    <div class="p-0 m-0 text-danger">
+                        <i class="bx bx-receipt"></i>
+                    </div>
+                    <div class="border-start ps-3 ms-3">
+                        <span id="total-surety-bond-not-paid">0</span><br>
+                        <small class="h6">Total Surety Bond Belum Lunas</small>
+                    </div>
+                </div>
+            </x-card>
+        </div>
+        <div class="col-md-4 mb-4">
+            <x-card>
+                <div class="d-flex display-5 align-items-center">
+                    <div class="p-0 m-0 text-success">
+                        <i class="bx bxs-receipt"></i>
+                    </div>
+                    <div class="border-start ps-3 ms-3">
+                        <span id="total-surety-bond-not-paid">0</span><br>
+                        <small class="h6">Total Surety Bond Lunas</small>
+                    </div>
+                </div>
+            </x-card>
+        </div>
+    </div>
+
+    <x-card class="mb-4">
+        <div class="income-chart-container">
+            <canvas id="chart"></canvas>
+        </div>
+    </x-card>
+
+    <x-card class="mb-4">
+        <div class="expense-chart-container">
+            <canvas id="chart"></canvas>
+        </div>
+    </x-card>
+
+    <x-card class="mb-4">
+        <div class="surety-bond-chart-container">
+            <canvas id="chart"></canvas>
+        </div>
+    </x-card>
+
+    <x-card class="mb-4">
+        <div class="guarantee-bank-chart-container">
+            <canvas id="chart"></canvas>
+        </div>
     </x-card>
 @endsection

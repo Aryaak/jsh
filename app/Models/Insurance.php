15a794044f00af8fa6fd05ea1fe14a794a5e693e
rosyidan.akmal <rosyidan.akmal@gmail.com>
2022-10-19 19:09:16 +0700

    Asuransi Done

--- app/Models/Insurance.php
+++ app/Models/Insurance.php
@@ -11,4 +11,25 @@ class Insurance extends Model
     use HasFactory;
 
     public $fillable = ['name','alias','address','pc_name','pc_position'];
+
+    private static function fetch(object $args): array{
+        return [
+            'name' => $args->name,
+            'alias' => $args->alias,
+            'address' => $args->address,
+            'pc_name' => $args->pc_name,
+            'pc_position' => $args->pc_position,
+        ];
+    }
+
+    public static function buat(array $params): self{
+        return self::create(self::fetch((object)$params));
+    }
+    public function ubah(array $params): bool{
+        return $this->update(self::fetch((object)$params));
+    }
+    public function hapus(): bool{
+        return $this->delete();
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DesignController;
 use App\Http\Controllers\BranchController;
+use App\Http\Controllers\InsuranceController;
 use App\Http\Controllers\RegionalController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,7 @@ Route::get('/', fn () => view('welcome'))->name('dashboard');
 Route::get('/uploader/tinymce', null)->name('uploader.tinymce'); // tolong disesuaikan ya
 Route::resource('branches',BranchController::class);
 Route::resource('regionals',RegionalController::class);
+Route::resource('insurances',InsuranceController::class);
 
 /**
  * -------------------------------------------------------------------------

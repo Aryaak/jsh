8a0eab0a8339720a7e7a3a07ead09fbb4dc6b781
Fathul Husnan <fathulhusnan9901@gmail.com>
2022-11-16 19:29:19 +0700

    fix migration unique on branch slug

--- database/migrations/2022_11_14_203555_make_slug_unique_on_branches_table.php
+++ database/migrations/2022_11_14_203555_make_slug_unique_on_branches_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('branches', function (Blueprint $table) {
             $table->string('slug')->unique()->change();
         });
     }

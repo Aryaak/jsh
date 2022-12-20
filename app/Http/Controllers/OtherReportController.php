977a74d39bfaa7e5c341321673150cbac2f7ac5b
ZD Aprillyo <zdaprillyo@gmail.com>
2022-12-20 22:25:58 +0700

    other profit is done and fixes footer sum

--- app/Http/Controllers/OtherReportController.php
+++ app/Http/Controllers/OtherReportController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Expense;
+use App\Models\Payment;
+use App\Models\SuretyBond;
+use App\Models\GuaranteeBank;
 use Illuminate\Http\Request;
+use DB;
 
 class OtherReportController
 {
-    //
+    public function profit(Request $request){
+        if($request->ajax()){
+            $toInsurance = DB::table('payments as pm')->join('insurances AS i','i.id','pm.insurance_id')
+            ->where('pm.type','regional_to_insurance')->where('pm.regional_id',auth()->id())->selectRaw("
+                CONCAT('Setoran ke ',i.name) AS title,pm.paid_at,0 AS debit,pm.total_bill AS credit
+            ");
+            $customExpense = DB::table('expenses as e')->where('e.regional_id',auth()->id())
+            ->selectRaw("e.title,e.transaction_date AS paid_at,0 AS debit,e.nominal AS 'credit'");
+
+            $profits = DB::table('instalments as i')->join('branches as b','b.id','i.branch_id')
+            ->selectRaw("CONCAT('Setoran Cabang ',b.name) AS title, i.paid_at,i.nominal AS debit,0 AS credit")
+            ->where('i.regional_id',auth()->id())
+            ->unionAll($toInsurance)
+            ->unionAll($customExpense)
+            ->get();
+            return datatables()->of($profits)
+            ->addIndexColumn()
+            ->toJson();
+        }
+        return view('report.other.profit');
+    }
 }
--- resources/views/report/guarantee-bank/product.blade.php
+++ resources/views/report/guarantee-bank/product.blade.php
@@ -153,11 +153,11 @@
                     var api = this.api();
                     // Remove the formatting to get integer data for summation
                     var intVal = function (i) {
-                        return typeof i === 'string' ? i.replace(/[\$,]/g, '') * 1 : typeof i === 'number' ? i : 0;
+                        return typeof i === 'string' ? i.replace(/[\$.]/g, '') * 1 : typeof i === 'number' ? i : 0;
                     };
                     let calculateCol = function(col){
                         return api.column(col, { page: 'current' }).data().reduce(function (a, b) {
-                            return intVal(a) + intVal(b);
+                            return parseFloat(intVal(a)) + parseFloat(intVal(b));
                         }, 0);
                     }
                     $(api.column(3).footer()).html(calculateCol(3));
